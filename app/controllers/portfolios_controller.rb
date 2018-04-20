8cf883deb8857e2f3554a8cad2713a7185455f9d
Judy He <jhe@sankynet.com>
2018-04-20 16:04:29 -0400

    implement edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,7 +8,6 @@ class PortfoliosController < ApplicationController
  end
 
  def create
-
   @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
   respond_to do |format|
@@ -19,4 +18,20 @@ class PortfoliosController < ApplicationController
     end
   end
  end
+
+ def edit
+   @portfolio_item = Portfolio.find(params[:id])
+ end
+
+ def update
+   @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Blog was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
 end
